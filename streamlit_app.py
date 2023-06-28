c460d8da9da60305288f56898be340c0212deae4
spoorthi-hegde <54339259+spoorthi-hegde@users.noreply.github.com>
2023-06-28 10:22:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit
-streamlit.title('I am a healthy DINER
-
-Breakfast MENU :
-Idli
-Dosa
-Poha')
+streamlit.title('I am a healthy DINER')
+streamlit.title('Breakfast MENU :')
+streamlit.title('Idli')
+streamlit.title('Poha')
