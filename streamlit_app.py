c5a19cb03a544119fc92e7b53f32003ffd75292c
spoorthi-hegde <54339259+spoorthi-hegde@users.noreply.github.com>
2023-06-28 10:56:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('I am a healthy DINER')
-streamlit.header('Breakfast MENU :')
-streamlit.text('Idli')
-streamlit.text('Poha')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
