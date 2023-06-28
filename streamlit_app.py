918b4195624dc3eea493c40379309292a635edb8
spoorthi-hegde <54339259+spoorthi-hegde@users.noreply.github.com>
2023-06-28 11:05:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 import streamlit
-streamlit.title('My parents are a healthy DINER')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.title('My Mom's new healthy DINER')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
